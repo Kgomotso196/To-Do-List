177830156c7276799ded3f8cb23cb5b6728383a3
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-06-09 10:48:19 +0200

    Add JavaScript functionality to display, edit, and delete tasks in To-Do-List

--- src/index.js
+++ src/index.js
@@ -1,20 +1,52 @@
+/*eslint-disable*/
+import _ from 'lodash'; /*eslint-disable*/
 import './style.css';
+import { Functionality } from './modules/functionality';
+const taskContainer = document.getElementById('first-section');
+const submitButton = document.getElementById('for-button');
+let editButtonStats=false;
+window.onload = function windowReady() {
+  Functionality.displayTask();
+  submitButton.onclick = function () {
+    Functionality.newTask();
+  };
 
-const todoTasks = [
-  { index: 1, type: 'Code Development', completed: true },
-  { index: 2, type: 'Code Testing', completed: false },
-  { index: 3, type: 'Code Maintenance', completed: true },
-];
+taskContainer.addEventListener('click', (e) => {
+  if (e.target !== null && e.target !== 'NaN' && e.target !== '') {
+    if (e.target.className === 'editButton') {
+      const ids = e.target.id.replace('editBttn-', '');
+      let description = document.getElementById('d' + ids);
+      let data = Functionality.getAllTasks();
+      const index = parseInt(ids, 10);
+      let editInput = document.getElementById('edit-'+ ids);
+      description.style.display='none';
+      editInput.style.display='block';
+      if (editButtonStats !== false) {
+        data[index-1].description=editInput.value;
+        description.style.display='block';
+        editInput.style.display='none';
+        Functionality.updateTask(data);
+        Functionality.displayTask();
+        editButtonStats = false;
+      }else{
+        editButtonStats = true;
+      }
+    }
+  }
+});
 
-const todoHolder = document.querySelector('.main-holder');
+ Functionality.displayTask();
+ taskContainer.addEventListener('click', (e) => {
+   if (e.target !== null && e.target !== 'NaN' && e.target !== '') {
+     if (e.target.className === 'deleteButton') {
+       const ids = e.target.id.replace('delete-', '');
+       const data = Functionality.getAllTasks();
+       const index = parseInt(ids, 10);
+       if (data !== []) {
+         Functionality.removeTask(index-1);
+       }
+     }
+   }
+ });
 
-const todos = todoTasks.map((todo) => `
-  <div class="todoTasks">
-    <div class="task"><input type="checkbox" id="checkBox" name="checkbox" value="1">
-      <label for="checkBox">${todo.type}</label>
-    </div>
-    <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-  </div>
-`).join('');
-
-todoHolder.innerHTML = todos;
+};
